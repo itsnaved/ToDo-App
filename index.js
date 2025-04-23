706f47499eb6db10a73a75f4fb13365e28b25d80
itsnaved <itsnaved11@gmail.com>
2025-04-23 17:52:09 +0530

    init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port=3000;
+const fs= require("fs");
 
 app.use(bodyParser.json());
 
@@ -93,6 +94,6 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
-app.listen(port,()=>{
+app.listen(port,()=>{ 
     console.log(`App listening`);
 })
