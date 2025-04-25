a9cd9765b745ceca81c2e2b2ff39e5976b788dec
itsnaved <itsnaved11@gmail.com>
2025-04-25 09:29:21 +0530

    making UI Dynamic

--- index.js
+++ index.js
@@ -1,99 +1,63 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const port=3000;
-const fs= require("fs");
+const express = require("express")
+const app= express();
+const bodyParser= require("body-parser");
+const cors = require("cors");
+const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
-function findIndex(arr, id) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].id === id) return i;
-  }
-  return -1;
+let todos =[];
+
+function findIndex(arr, ind){
+    for(let i=0; i< arr.length; i++){
+        if(arr[i].id === ind){
+            return i;
+        }
+    }
+    return -1;
 }
 
-function removeAtIndex(arr, index) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== index) newArray.push(arr[i]);
-  }
-  return newArray;
+function removeIndex(arr, index){
+    let newArray= [];
+    for(let i=0; i< arr.length; i++){
+        if(i !== index){
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
 }
 
-app.get("/todo",(req,res)=>{
-    fs.readFile("todos.json","utf-8", (err, data)=>{
-        if(err) throw(err)
-            else{
-                var answer = JSON.parse(data);
-                res.json(answer);
-            }
-    })
-    
+app.get("/todo",(req, res)=>{
+    res.json(todos);
 });
 
-// app.get('/todos/:id', (req, res) => {
-//   const todoIndex = findIndex(todos, parseInt(req.params.id));
-//   if (todoIndex === -1) {
-//     res.status(404).send();
-//   } else {
-//     res.json(todos[todoIndex]);
-//   }
-// });
-
-app.post("/todo",(req,res)=>{
-    
+app.post("/todo",(req, res)=>{
     let newTodo={
-        id: Math.floor(Math.random() * 1000000),
+        id: Math.floor(Math.random()* 10000),
         title: req.body.title,
         description: req.body.description
     }
-    fs.readFile("todos.json","utf-8", (err, data)=>{
-        if(err) throw (err);
-        const todos = JSON.parse(data);
-        todos.push(newTodo);
-        fs.writeFile("todos.json", JSON.stringify(todos),(err)=>{
-            if(err) throw err; 
-            res.status(201).json(newTodo);
-        })
-    })
-    
-});
-
-
-// app.put('/todos/:id', (req, res) => {
-//   const todoIndex = findIndex(todos, parseInt(req.params.id));
-//   if (todoIndex === -1) {
-//     res.status(404).send();
-//   } else {
-//     todos[todoIndex].title = req.body.title;
-//     todos[todoIndex].description = req.body.description;
-//     res.json(todos[todoIndex]);
-//   }
-// });
-
-app.delete("/todo/:id",(req, res)=>{
-   fs.readFile("todos.json","utf-8",(err,data)=>{
-    if(err)throw err;
-    let todos = JSON.parse(data)
-    const todosIndex = findIndex(todos, parseInt(req.params.id));
-    if(todosIndex === -1){
-        res.status(404).send("cannot find todo");
-    }else{
-        todos = removeIndex(todos, todosIndex);
-        fs.writeFile("todos.json", JSON.stringify(todos), (err)=>{
-            if(err) throw err;
-            res.status(200).send("Todo deleted succesfull");
-        })
-    }
-   })
-
+    todos.push(newTodo);
+    res.status(200).json(newTodo);
 })
 
-app.use((req, res, next) => {
-  res.status(404).send();
-});
+app.delete("/todo/:id",(req, res)=>{
+    let id= parseInt(req.params.id);
+    let index= findIndex(todos, id);
+    if(index === -1){
+        res.status(404).send("Todo Id Not found");
+    }
+    else{
+        todos = removeIndex(todos, index);
+        res.status(200).send("deleted succesfully");
+    }
+})
 
-app.listen(port,()=>{ 
-    console.log(`App listening`);
+app.use((req, res, next)=>{
+    res.status(404).send("Route not found");
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on Port ${port}`);
 })
