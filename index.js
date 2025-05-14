a68600fedeac4cc06f04400048e19079a6311e93
itsnaved <itsnaved11@gmail.com>
2025-05-15 01:11:48 +0530

    todos routes complete

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
-const express = require("express")
-const app= express();
-const bodyParser= require("body-parser");
-const cors = require("cors");
-const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors= require("cors");
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-let todos =[];
+let todos = [];
 
-function findIndex(arr, ind){
-    for(let i=0; i< arr.length; i++){
-        if(arr[i].id === ind){
-            return i;
-        }
-    }
-    return -1;
-}
-
-function removeIndex(arr, index){
-    let newArray= [];
-    for(let i=0; i< arr.length; i++){
-        if(i !== index){
-            newArray.push(arr[i]);
-        }
-    }
-    return newArray;
-}
-
-app.get("/todo",(req, res)=>{
-    res.json(todos);
+app.get('/todos', (req, res) => {
+  res.json(todos);
 });
 
-app.post("/todo",(req, res)=>{
-    let newTodo={
-        id: Math.floor(Math.random()* 10000),
-        title: req.body.title,
-        description: req.body.description
-    }
-    todos.push(newTodo);
-    res.status(200).json(newTodo);
-})
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+});
 
-app.delete("/todo/:id",(req, res)=>{
-    let id= parseInt(req.params.id);
-    let index= findIndex(todos, id);
-    if(index === -1){
-        res.status(404).send("Todo Id Not found");
-    }
-    else{
-        todos = removeIndex(todos, index);
-        res.status(200).send("deleted succesfully");
-    }
-})
+app.post('/todos', (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000), // unique random id
+    title: req.body.title,
+    description: req.body.description
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
 
-app.use((req, res, next)=>{
-    res.status(404).send("Route not found");
-})
+app.put('/todos/:id', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.json(todos[todoIndex]);
+  }
+});
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on Port ${port}`);
-})
+app.delete('/todos/:id', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+app.listen(3000);
